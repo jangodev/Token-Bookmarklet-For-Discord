7fce69669a2896f74e5a568c7bbf86fbe727bf4f
jangomango <86420004+jangodev@users.noreply.github.com>
2023-06-01 12:43:14 -0400

    notes

--- login.js
+++ login.js
@@ -1,1 +1,2 @@
+// warning: educational purposes only. Paste this into the discord console.
 location.reload();var i = document.createElement('iframe');document.body.appendChild(i);i.contentWindow.localStorage.token = "\"token_here\""
